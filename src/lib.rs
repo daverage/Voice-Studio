8bcf7499d1419944f89f580c9ba0ad39fb3a7ae8
Andrzej Marczewski <daverage@gmail.com>
2026-02-05 09:52:50 +0000

    Buttons in a new place

--- src/lib.rs
+++ src/lib.rs
@@ -628,7 +628,7 @@ impl Plugin for VoiceStudioPlugin {
     const VENDOR: &'static str = "Andrzej Marczewski";
     const URL: &'static str = "";
     const EMAIL: &'static str = "";
-    const VERSION: &'static str = "0.6.4";
+    const VERSION: &'static str = "0.6.5";
 
     const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
         main_input_channels: NonZeroU32::new(2),
